9b35ac5dd4ed07a2a7bff4fa21bf958fad84ad45
Miguel Ramon <nozerorma@gmail.com>
2023-02-15 19:27:04 +0100

    added if logic to ref curation

--- scripts/jun_reference_parser.py
+++ scripts/jun_reference_parser.py
@@ -3,11 +3,11 @@
 
 import sys
 import os
-
 import sqlite3
-
-# For compressed input files
 import re
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # think about creating table inside def declaration
 CLINVAR_REFERENCE_DEFS = [
@@ -15,15 +15,14 @@ CLINVAR_REFERENCE_DEFS = [
 CREATE TABLE IF NOT EXISTS reference (
     ventry_id INTEGER PRIMARY KEY AUTOINCREMENT,
     allele_id INTEGER NOT NULL,
-    citation_source VARCHAR(64),
-    citation_id VARCHAR(64)
+    citation_source VARCHAR(64) NOT NULL,
+    citation_id VARCHAR(16)
 )
 """
 ]
 
 # EN PRIMERA INSTANCIA VOY A PSEUDOCOPIAR EL CODIGO DEL PROFE
 # CUANDO ENTIENDA QUE PASA, LO HAGO ORIGINAL
-
 
 def open_clinvar_db(db_file):
     db = sqlite3.connect(db_file)
@@ -33,16 +32,16 @@ def open_clinvar_db(db_file):
         for tableDecl in CLINVAR_REFERENCE_DEFS:
             cur.execute(tableDecl)
     except sqlite3.Error as e:
-        print("An error occurred: {}".format(str(e)), file=sys.stderr)
-    finally:
+            logging.error("An error occurred: %s", str(e))
+    finally:    
         cur.close()
 
     return db
 
 
-def store_reference_file(db, reference_file):
+def store_clinvar_ref(db, reference_file):
     with open(reference_file, "rt") as ref:
-        # headerMapping = None # no need?
+        headerMapping = None # no need?
         # known_genes = set() # no use AON
         cur = db.cursor()
 
@@ -75,7 +74,6 @@ def store_reference_file(db, reference_file):
                     # And extracting what we really need
                     # Table variation
                     # change for the required vals
-                    # ventry somewhere in here? note that for autoincrement, for_keys must be somewhere declared
                     allele_id = int(columnValues[headerMapping["AlleleID"]])
                     citation_source = columnValues[headerMapping["citation_source"]]
                     citation_id = columnValues[headerMapping["citation_id"]]
@@ -90,20 +88,45 @@ def store_reference_file(db, reference_file):
 
                     ventry_id = cur.lastrowid
 
-    def ref_curation(db, reference_file):
-    # now, here should go the curation definition
-        # for it to work, select from left
-        with open(reference_file, "rt") as ref:
-            cur = db.cursor()
+def ref_curation(db, reference_file):
+    cur = db.cursor()
+    with db:
+        cur.execute("""
+            SELECT name
+            FROM sqlite_master
+            WHERE type='table'
+            AND name='variant'
+        """
+        )
+        isPresent = cur.fetchone()
 
-            with db:
-                try:
-                    cur.exectue("""
-                        SELECT *
-                        FROM variant v
-                        JOIN references r
-                        ON v.allele_id = r.allele_id;
-                    """)
-                    # marcos introduce SELECT MAX y LEFT JOIN, ver
-                    last_citation=cur.fetchall() #marcos
-                except:
+        if isPresent:
+            #this is a temp solution prior to table curation
+            print("Variant table could be fetched from {db_file}")
+        
+        else:
+            print("Variant table could not be fetched from {db_file}")
+
+        cur.close()    
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print("Usage: {0} {{database_file}} {{compressed_clinvar_file}}".format(sys.argv[0]), file=sys.stderr)
+		sys.exit(1)
+
+	# La siguiente línea asigna el primer argumento pasado al script a la variable "db_file" 
+	# y el segundo argumento a la variable "clinvar_file".
+	db_file = sys.argv[1]
+	clinvar_file = sys.argv[2]
+
+	# Luego, se llama a la función "open_clinvar_db" pasándole la ruta del archivo de la base de datos. 
+	# Esta función abre la conexión con la base de datos y devuelve una conexión.
+	db = open_clinvar_db(db_file)
+
+	# La función "store_clinvar_file" se llama con los argumentos "db" y "clinvar_file". 
+	# Esta función almacena los datos del archivo "clinvar_file" en la base de datos.
+	store_clinvar_ref(db,clinvar_file)
+
+    # Finalmente, la conexión a la base de datos se cierra mediante el método "close()".
+	db.close()
